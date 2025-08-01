30e1f8b0e6f683c49e1b7dc6928249f631dc423a
Alexander Schied <schiedalex96@gmail.com>
2025-08-01 09:31:08 +0200

    Test Program

---
+++ MonoxhunderTest/Program.cs
@@ -0,0 +1,6 @@
+﻿using Monoxhunder;
+using Microsoft.Xna.Framework;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Starting Test");
+Line line = new Line(new Vector2(0, 0), new Vector2(14,2));
+Console.WriteLine(line.Lenght);
