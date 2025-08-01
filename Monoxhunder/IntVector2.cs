e5909d46727c3c52fa2092dc16e19316594f1e36
Alexander Schied <schiedalex96@gmail.com>
2025-08-01 16:39:16 +0200

    IntVector2 operators

--- Monoxhunder/IntVector2.cs
+++ Monoxhunder/IntVector2.cs
@@ -18,5 +18,35 @@ namespace Monoxhunder
             X = (int)Math.Floor(vector.X);
             Y = (int)Math.Floor(vector.Y);
         }
+
+        public static IntVector2 operator +(IntVector2 a, IntVector2 b)
+        {
+            return new IntVector2(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static IntVector2 operator -(IntVector2 a, IntVector2 b)
+        {
+            return new IntVector2(a.X - b.X, a.Y - b.Y);
+        }
+
+        public static IntVector2 operator *(IntVector2 a, IntVector2 b)
+        {
+            return new IntVector2(a.X * b.X, a.Y * b.Y);
+        }
+
+        public static Vector2 operator /(IntVector2 a, IntVector2 b)
+        {
+            return new Vector2(a.X / b.X, a.Y / b.Y);
+        }
+
+        public static IntVector2 operator *(IntVector2 a, int b)
+        {
+            return new IntVector2(a.X * b, a.Y * b);
+        }
+
+        public static Vector2 operator /(IntVector2 a, int b)
+        {
+            return new Vector2(a.X / b, a.Y / b);
+        }
     }
 }
