200770da2a86c27af1e1e83e4274bb89f800742c
Alexander Schied <schiedalex96@gmail.com>
2025-08-09 09:52:31 +0200

    PriorityQueue Test

--- MonoxhunderTest/Program.cs
+++ MonoxhunderTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Monoxhunder;
+using Monoxhunder.Collections;
 using MonoxhunderTest;
 
 Console.WriteLine("Starting Test");
@@ -8,3 +9,12 @@ TestUtil.TestLineGeneration(new IntVector2(0, 0), new IntVector2(5, 2), [new(0, 
 
 TestUtil.TestLineGeneration(new IntVector2(4, 0), new IntVector2(0, 4), [new(4, 0), new(3, 1), new(2, 2), new(1, 3), new(0, 4)]);
 TestUtil.TestLineGeneration(new IntVector2(0, 4), new IntVector2(4, 0), [new(0, 4), new(1, 3), new(2, 2), new(3, 1), new(4, 0)]);
+
+PriorityQueue<float> priorityQueue = new();
+priorityQueue.Add(100);
+priorityQueue.Add(3);
+priorityQueue.Add(20);
+priorityQueue.Add(-4);
+priorityQueue.Add(0.3f);
+
+Console.WriteLine(priorityQueue.ToString());
